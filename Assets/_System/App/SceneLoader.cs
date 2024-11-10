81803df5a858fa7a24abc7cdbe0bcb40c57af430
Adam Adhar <adamadhar28@gmail.com>
2024-11-10 18:39:39 +0100

    + System folder +App folder +Scene Loader

---
+++ Assets/_System/App/SceneLoader.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SceneLoader : MonoBehaviour
+{
+    public static SceneLoader I;
+
+    [SerializeField]
+    private GameObject _loadingScreen;
+
+    [SerializeField]
+    private Slider _progressBar;
+
+    private Coroutine _loadingCoroutine;
+
+    private void Awake()
+    {
+        if (I == null)
+        {
+            I = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        if (SceneManager.GetActiveScene().name == sceneName)
+            return;
+
+        if (_loadingCoroutine != null)
+            return;
+
+        _loadingCoroutine = StartCoroutine(LoadSceneAsync(sceneName));
+    }
+
+    private IEnumerator LoadSceneAsync(string sceneName)
+    {
+        if (_loadingScreen != null)
+            _loadingScreen.SetActive(true);
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Single);
+        asyncLoad.allowSceneActivation = false;
+
+        while (!asyncLoad.isDone)
+        {
+            if (_progressBar != null)
+                _progressBar.value = Mathf.Clamp01(asyncLoad.progress / 0.9f);
+
+            if (asyncLoad.progress >= 0.9f)
+            {
+                yield return new WaitForSeconds(1f);
+                asyncLoad.allowSceneActivation = true;
+            }
+
+            yield return null;
+        }
+
+        if (_loadingScreen != null)
+            _loadingScreen.SetActive(false);
+
+        _loadingCoroutine = null;
+    }
+}
