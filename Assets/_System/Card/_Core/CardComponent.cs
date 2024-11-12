ea28368ab9ea781b4634ff5529e4e27f9d6ea030
Troupote <thybault.jallu@icloud.com>
2024-11-12 13:27:36 +0100

    + CardComponent

--- Assets/_System/Card/_Core/CardComponent.cs
+++ Assets/_System/Card/_Core/CardComponent.cs
@@ -2,6 +2,18 @@ using UnityEngine;
 
 public class CardComponent : MonoBehaviour
 {
+    [SerializeField]
+    private CardSO _cardSO;
+    private string _name;
+    private Color _color;
+    private int _price;
+    private int _activationNumber;
+
+    public void Effect()
+    {
+        _cardSO.ApplyEffect();
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
