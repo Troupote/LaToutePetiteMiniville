980d89fc5bb775e4c30ff281abc804a87f533e94
Adam Adhar <adamadhar28@gmail.com>
2024-11-12 23:22:18 +0100

    + Player Component + IA Component

---
+++ Assets/_System/Entity/Player/PlayerComponent.cs
@@ -0,0 +1,10 @@
+/// <summary>
+/// Represents a player in runtime.
+/// </summary>
+public class PlayerComponent : EntityComponent
+{
+    private void Start()
+    {
+        base.Init();
+    }
+}
