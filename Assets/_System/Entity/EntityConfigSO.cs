37ee5f5cea6b1454e4309dcbc7687fe0163b897b
Adam Adhar <adamadhar28@gmail.com>
2024-11-13 00:37:02 +0100

    # Split Entity config between EntityConfigSO and GameConfigSO

--- Assets/_System/Entity/EntityConfigSO.cs
+++ Assets/_System/Entity/EntityConfigSO.cs
@@ -5,10 +5,6 @@ public class EntityConfigSO : ScriptableObject
 {
     #region Fields
 
-    [Tooltip("The number of dices the entity has to play with.")]
-    [SerializeField]
-    private int _nbDices = 0;
-
     [Tooltip("The number of coins the entity has at the beginning.")]
     [SerializeField]
     private int _coins = 0;
@@ -16,9 +12,6 @@ public class EntityConfigSO : ScriptableObject
     #endregion
 
     #region Public API
-
-    ///<inheritdoc cref="_nbDices"/>
-    public int NbDices => Mathf.Max(0, _nbDices);
 
     ///<inheritdoc cref="_coins"/>
     public int Coins => Mathf.Max(0, _coins);
