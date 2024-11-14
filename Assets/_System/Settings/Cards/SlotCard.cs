d5c3b66cc288c0054ae9a055330fa28f8e156385
ChaiseEnFer <maxime.brunel.lycee@gmail.com>
2024-11-14 16:43:32 +0100

    SlotCard

--- Assets/_System/Settings/Cards/SlotCard.cs
+++ Assets/_System/Settings/Cards/SlotCard.cs
@@ -1,16 +1,8 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SlotCard : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public CardSO Card;
+    public Button Button;
 }
