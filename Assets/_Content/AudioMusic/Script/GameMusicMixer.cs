cdd7b762681de9bfdf4e7458009ea8d66c043139
Adam Adhar <adamadhar28@gmail.com>
2024-11-14 19:51:47 +0100

    + Music mixer

---
+++ Assets/_Content/AudioMusic/Script/GameMusicMixer.cs
@@ -0,0 +1,22 @@
+using DG.Tweening;
+using UnityEngine;
+
+public class GameMusicMixer : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource MusicSource = null;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        MusicSource.volume = 0;
+        MusicSource.Play();
+        MusicSource.DOFade(0.6f, 1.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
