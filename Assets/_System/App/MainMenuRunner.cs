fb64f89cab4e92a0368ea04cb232ca64a150ea0f
Troupote <thybault.jallu@icloud.com>
2024-11-14 22:45:20 +0100

    # :devTool: add a slider for menu music

--- Assets/_System/App/MainMenuRunner.cs
+++ Assets/_System/App/MainMenuRunner.cs
@@ -7,6 +7,13 @@ public class MainMenuRunner : MonoBehaviour
     #region Fields
     [SerializeField]
     private AudioSource MenuMusic;
+
+    [SerializeField]
+    [Range(0f, 1f)]
+    private float MenuVolume;
+
+    [SerializeField]
+    private AudioSource ButtonSound;
     #endregion
 
     #region Public API
@@ -15,24 +22,27 @@ public class MainMenuRunner : MonoBehaviour
     {
         MenuMusic.volume = 0;
         MenuMusic.Play();
-        MenuMusic.DOFade(1, 1);
+        MenuMusic.DOFade(MenuVolume, 1);
     }
 
     public void Play()
     {
         Debug.Log("Menu - Play");
         AppManager.Instance.Play();
+        ButtonSound.Play();
         MenuMusic.DOFade(0, 1);
     }
     
     public void Credits()
     {
         AppManager.Instance.Credits();
+        ButtonSound.Play();
     }
     
     public void Quit()
     {
         AppManager.Instance.Quit();
+        ButtonSound.Play();
     }
 
     #endregion
