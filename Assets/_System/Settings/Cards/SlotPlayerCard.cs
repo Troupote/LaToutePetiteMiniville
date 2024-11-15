2318a5ceb1aa17a8263bf88eca03a272943f7ad7
ChaiseEnFer <maxime.brunel.lycee@gmail.com>
2024-11-15 16:36:42 +0100

    + modify prefabof slot card

--- Assets/_System/Settings/Cards/SlotPlayerCard.cs
+++ Assets/_System/Settings/Cards/SlotPlayerCard.cs
@@ -9,19 +9,37 @@ public class SlotPlayerCard : MonoBehaviour
     [SerializeField]
     private GameObject _positionOfCardBuy;
 
-    [SerializeField] private Transform _targetPosition;  // Position cible de l'objet
-    [SerializeField] private float _duration = 1.0f;      // Durée du déplacement
+    [SerializeField]
+    private RectTransform _targetPosition;  // Position cible de l'objet
+
+    [SerializeField]
+    private float _duration = 1.0f;      // Durée du déplacement
+
+    private MooveCurrentPositionCardHandPlayer _handPlayer;
+
+    private EntityComponent _entityComponent;
+
+
+
+    Transform target=null;  
+
+    public void SetPlayer(EntityComponent player)
+    {
+        _entityComponent = player;
+    }
 
     public void AddCarteOnScene() 
     {
+        if (_entityComponent is PlayerComponent)
+            target.position = FindFirstObjectByType<PlayerDeckContainer>().transform.position;
+
         Vector3 position = _positionOfCardBuy.transform.position;
         GameObject CreatedObject = Instantiate(_printerPrefab, position, Quaternion.identity);
+
+        transform.DOMove(target.position, _duration);
     }
 
-    /*
-    public void MooveCardInHandPlayer() 
-    {
-       transform.DOMove(new Vector3(
-    }
-    */
+
+
+    
 }
