09b99ecc1fd61826aa5e34e2881a5da6b3d96bec
Adam Adhar <adamadhar28@gmail.com>
2024-11-16 02:47:13 +0100

    # rename DiceManagerScript > DiceManager

---
+++ Assets/_System/Dice/DiceManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class DiceManager : MonoBehaviour
+{
+    public GameObject diceObject;
+    public int nbDiceRolling = 0;
+    public int resultFinal = 0;
+    public bool diceLaunch = false;
+
+    public void CreateDice(int nbDice)
+    {
+        diceLaunch = true;
+        nbDiceRolling = nbDice;
+        for (int i = 0; i < nbDice; i++)
+        {
+            Debug.Log("Dice value");
+            Instantiate(diceObject);
+        }
+    }
+    public void Update()
+    {
+        if (diceLaunch && nbDiceRolling == 0)
+            resultFinal = DiceCheckZoneScript.finalCount;
+    }
+}
