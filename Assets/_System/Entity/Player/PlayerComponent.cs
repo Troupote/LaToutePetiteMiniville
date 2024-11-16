74c69414b31d1673216d5402ea0c7415c6448ef2
Adam Adhar <adamadhar28@gmail.com>
2024-11-16 02:41:08 +0100

    + Test data entity component

--- Assets/_System/Entity/Player/PlayerComponent.cs
+++ Assets/_System/Entity/Player/PlayerComponent.cs
@@ -1,10 +1,17 @@
+using UnityEngine;
+
 /// <summary>
 /// Represents a player in runtime.
 /// </summary>
 public class PlayerComponent : EntityComponent
 {
+    [SerializeField]
+    CardSO cardTest = null;
+
     private void Start()
     {
         base.Init();
+
+        BuyCard(cardTest);
     }
 }
